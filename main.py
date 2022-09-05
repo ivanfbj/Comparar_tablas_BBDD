d6e46e054422083afb6e58a19d4dcaa3b977d8d7
ivanfbj <ivanfbj@gmail.com>
2022-09-05 00:10:01 -0500

    implementar create table con sqlalchemy
    Implementar que se cree la tabla en la base de datos si no existe y un contador de registros

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 import querys_sql as sql
 from create_csv import create_csv
 import bulk_insert_database as bki
+import connection_data_base as db
+from sqlalchemy import MetaData, Table, Column, Integer, String
+import sqlalchemy
+
 
 # Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     database_name = 'AdventureWorks2019'
     server_name = 'AGN5\SQLEXPRESS'
@@ -11,6 +16,24 @@ if __name__ == '__main__':
     # print(name_file)
     # print(file_abspath)
 
+    engine = db.windows_authentication_sqlalchemy(server_name, 'appTest')
+    if not sqlalchemy.inspect(engine).has_table(name_file.replace('.csv', '')):
+        metadata = MetaData(engine)
+        Table(name_file.replace('.csv', ''), metadata,
+              Column('ID', Integer, primary_key=True, nullable=False, autoincrement=False),
+              Column('TABLE_SCHEMA', String),
+              Column('TABLE_NAME', String)
+              )
+        metadata.create_all()
+
+    meta_data = MetaData(engine)
+    sqlalchemy.MetaData.reflect(meta_data)
+    actor_table = meta_data.tables[name_file.replace('.csv', '')]
+
+    result = sqlalchemy.select([sqlalchemy.func.count()]).select_from(actor_table).scalar()
+
+    print("Count:", result)
+    print('inicio bulk')
     bki.bulk_insert_csv(server_name, 'appTest', name_file.replace('.csv', ''), file_abspath)
 
     # name_file, file_abspath = create_csv(server_name, database_name, sql.list_all_columns_tables(), 'columns_tables')
