260f61044c78f6c566f35bf6b64ae777c6f054cd
ivanfbj <ivanfbj@gmail.com>
2022-09-07 20:10:06 -0500

    Implementar truncate si la tabla tiene datos

--- bulk_insert_database.py
+++ bulk_insert_database.py
@@ -1,14 +1,18 @@
 import connection_data_base as db
+import time
 
 def bulk_insert_csv(server_name, database_name, file_and_table_name, file_abspath):
     connect_database_pyodbc = db.windows_authentication_pyodbc(server_name, database_name)
 
     cursor = connect_database_pyodbc.cursor()
-
-    cursor.execute(
-        f"BULK INSERT {file_and_table_name} FROM '{file_abspath}' WITH(FIELDTERMINATOR='|', ROWTERMINATOR='\n', FIRSTROW=2)")
-    connect_database_pyodbc.commit()
-
+    try:
+        print(f'Hora Inicio Bulk: {time.strftime("%H:%M:%S")}')
+        cursor.execute(
+            f"BULK INSERT {file_and_table_name} FROM '{file_abspath}' WITH(FIELDTERMINATOR='|', ROWTERMINATOR='\n', FIRSTROW=2)")
+        connect_database_pyodbc.commit()
+        print(f'Hora Fin Bulk: {time.strftime("%H:%M:%S")}')
+    except Exception as ex:
+        print (f'Error al realizar bulk insert en {file_and_table_name}: {ex}')
     cursor.close()
     connect_database_pyodbc.close()
 
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 import querys_sql as sql
 from create_csv import create_csv
 import bulk_insert_database as bki
 import connection_data_base as db
 from sqlalchemy import MetaData, Table, Column, Integer, String
 import sqlalchemy
-
+from sqlalchemy.orm import sessionmaker
 
 # Press the green button in the gutter to run the script.
 
@@ -13,13 +12,14 @@ if __name__ == '__main__':
     database_name = 'AdventureWorks2019'
     server_name = 'AGN5\SQLEXPRESS'
     name_file, file_abspath = create_csv(server_name, database_name, sql.list_all_tables(), 'tables')
+    table_name = name_file.replace('.csv', '')
     # print(name_file)
     # print(file_abspath)
 
     engine = db.windows_authentication_sqlalchemy(server_name, 'appTest')
     if not sqlalchemy.inspect(engine).has_table(name_file.replace('.csv', '')):
         metadata = MetaData(engine)
-        Table(name_file.replace('.csv', ''), metadata,
+        Table(table_name, metadata,
               Column('ID', Integer, primary_key=True, nullable=False, autoincrement=False),
               Column('TABLE_SCHEMA', String),
               Column('TABLE_NAME', String)
@@ -28,16 +28,21 @@ if __name__ == '__main__':
 
     meta_data = MetaData(engine)
     sqlalchemy.MetaData.reflect(meta_data)
-    actor_table = meta_data.tables[name_file.replace('.csv', '')]
+    list_tables_table = meta_data.tables[table_name]
 
-    result = sqlalchemy.select([sqlalchemy.func.count()]).select_from(actor_table).scalar()
+    result = sqlalchemy.select([sqlalchemy.func.count()]).select_from(list_tables_table).scalar()
+    if result > 0:
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        session.execute(f'''TRUNCATE TABLE {table_name}''')
+        session.commit()
+        session.close()
+        print(f'Tabla {table_name} borrada correctamente')
 
     print("Count:", result)
-    print('inicio bulk')
     bki.bulk_insert_csv(server_name, 'appTest', name_file.replace('.csv', ''), file_abspath)
 
     # name_file, file_abspath = create_csv(server_name, database_name, sql.list_all_columns_tables(), 'columns_tables')
     # print(name_file)
     # print(file_abspath)
     # cursor.close()
-
